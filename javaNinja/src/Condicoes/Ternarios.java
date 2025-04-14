b50eafcab8734c02b4c2037faef4c8c8f4eba817
dssfelipe <164665252+dssfelipe@users.noreply.github.com>
2025-04-14 17:27:22 -0300

    ternarios
    forma de economizar linhas de código, em estrutura condicional.

---
+++ javaNinja/src/Condicoes/Ternarios.java
@@ -0,0 +1,13 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+        /*
+        Ternarios: São maneiras de reduzir o codigo
+        variavel = (condição) ? valorSeVerdadeiro : valorSeFalso;
+         */
+
+        short numeroDeMissoes = 11;
+        String nivel = (numeroDeMissoes >= 10) ? "Esse ninja está com mais de 10 missoes" : "esse ninja tem menos de 10 missoes";
+    }
+}
